f92facd6a14e78b50f245810d4696125a49eee11
godsman <godsman@dreamwiz.com>
2016-09-22 19:49:45 +0900

    sourcetree 첫 사용

---
+++ src/main/java/net/ssp/MySspApplication.java
@@ -0,0 +1,12 @@
+package net.ssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySspApplication.class, args);
+	}
+}
