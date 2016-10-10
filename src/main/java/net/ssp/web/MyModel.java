a162319b7e3fc64c2e7e02b6a3bd127e9241187c
godsman <godsman@dreamwiz.com>
2016-10-10 09:50:36 +0900

    html template 추가

---
+++ src/main/java/net/ssp/web/MyModel.java
@@ -0,0 +1,19 @@
+package net.ssp.web;
+
+public class MyModel
+{
+	private String name;
+
+	public MyModel(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+	
+	
+	
+}
--- src/main/java/net/ssp/web/WelcomeController.java
+++ src/main/java/net/ssp/web/WelcomeController.java
@@ -1,5 +1,9 @@
 package net.ssp.web;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController
 {
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model) {
-		System.out.println("name : " + name + "age : " + age);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String welcome(Model model) {
+//		List<MyModel> repo = new ArrayList<MyModel>();
+		List<MyModel> repo = Arrays.asList(new MyModel("javajigi"), new MyModel("hub"), new MyModel("rip"));
+		
+		model.addAttribute("repo", repo);
 		return "welcome"; //welcome.html
 	}
 }
